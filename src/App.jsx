c596671c273f7d263e10727e6b3b6baafec26757
Chandujeevan <jeevanbalasubramanyam09gmail.com>
2026-07-29 15:16:42 +0530

    fix : fix the domain configuration

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Movie-Explorer" element={<Home />} />
           <Route path="/movie-explorer" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
