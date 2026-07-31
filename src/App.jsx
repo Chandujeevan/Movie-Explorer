b71f579f11045c074c6b4b5c3ad1c1ad9e3bc1cc
Chandujeevan <jeevanbalasubramanyam09gmail.com>
2026-07-31 19:17:51 +0530

    feat: add favorites functionality with local storage persistence

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./css/App.css";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import { Routes, Route } from "react-router-dom";
+import { MovieProvideer } from "./contexts/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <MovieProvideer>
       <NavBar/>
       <main className="main-content">
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvideer>
   );
 }
 
